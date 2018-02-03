a96f1db72d01c11cf879cf11706d012eb7d9956e
Kacper Kowalski <repcak95@gmail.com>
2018-02-03 22:51:27 +0100

    day 4 challenge

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,13 @@
 //Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+  const browser = req.headers['user-agent'];
+  console.log(browser);
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.end(browser);
+});
+
+srv.listen(3000, () => {
+  console.log('server uruchomiony na porcie 3000');
+});
